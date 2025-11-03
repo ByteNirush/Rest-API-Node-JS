d439ca9e13eb3451b9edfc79ed2997a4e5b40d42
ByteNirush <nirushstha@gmail.com>
2025-11-03 08:14:14 +0545

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const connectDB = require("./db");
 const User = require("./models/User");
+require("dotenv").config();
 
 const app = express();
 connectDB();
@@ -10,29 +11,28 @@ app.use(express.json());
 
 // Register API
 app.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    try {
-        // check if user exists
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ message: "Email already registered" });
-        }
-
-        // hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Create user
-        user = new User({ name, email, password: hashedPassword });
-        await user.save();
-
-        res.status(201).json({ message: "User registered successfully" });
-    } catch (err) {
-        res.status(500).json({ message: "Server error" });
+  try {
+    // check if user exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already registered" });
     }
-});
 
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 // Login API
 
@@ -44,7 +44,8 @@ app.post("/login", async (req, res) => {
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
 
     res.json({ message: "Login successful" });
   } catch (err) {
@@ -52,5 +53,5 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
